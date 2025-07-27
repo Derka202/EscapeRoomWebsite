0a30e43c913c495cb561260196775c5ef19f68d6
Peter G <ptqgallagher@gmail.com>
2025-07-26 21:54:18 -0500

    added a script to read the footer info from the site_info.json rather than hardcoding it on individual pages

---
+++ populate_footer.js
@@ -0,0 +1,23 @@
+function load_footer_data() {
+    fetch('site_info.json')
+        .then(response => response.json())
+        .then(data => {
+            const footer = data.find(d => d.data === "footer_info")
+            if (!footer) {
+                console.error("Room not found:", name);
+                return;
+            }
+        
+            const date = footer.edit_date;
+            const creator = footer.creator;
+            const dateContainer = document.getElementById('edit_date');
+            const creatorContainer = document.getElementById('creator');
+            dateContainer.textContent = `Updated on ${date}`;
+            creatorContainer.textContent = `Website created by ${creator}`;
+        }
+    );
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    load_footer_data();
+}); 
